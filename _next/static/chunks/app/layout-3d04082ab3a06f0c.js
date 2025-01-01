dc2d1e56716c2b4241d61b787dd89d24576db3f4
mobyw <mobyw@outlook.sg>
2025-01-01 18:59:28 +0000

    Deploying to gh-pages from @ swjtuhub/homepage@bbb72996b1150ffd6af8b519573cdf53058334a7 🚀

---
+++ _next/static/chunks/app/layout-3d04082ab3a06f0c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{7889:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,7831,23)),Promise.resolve().then(l.t.bind(l,1789,23))},1789:()=>{},7831:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_d65c78"}}},e=>{var s=s=>e(e.s=s);e.O(0,[216,921,871,358],()=>s(7889)),_N_E=e.O()}]);
